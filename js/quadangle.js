ce815a6cc2c37095fe1de0242f73856600122a2a
Jialinnnn <86464561+Jialinnnn@users.noreply.github.com>
2021-09-24 16:13:11 +0800

    Add files via upload

---
+++ js/quadangle.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init(){
+	var canvas = document.getElementById( "triangle-canvas" );
+	gl = WebGLUtils.setupWebGL( canvas );
+	if( !gl ){
+		alert( "WebGL isn't available" );
+	}
+
+	// 顶点数以及顶点位置的定义
+	var vertices = [
+		/*-1.0, -1.0, 
+		 0.0,  1.0, 
+		 1.0, -1.0*/
+		 0.0, -1.0,
+		 1.0, -1.0,
+		 1.0,  1.0,
+		 0.0, -1.0,
+		 1.0,  1.0,
+		 -1.0,  1.0,
+		 /*-0.5, -0.5,
+		 0.0, 0.5,
+		 0.5, -0.5*/
+	];
+
+	// WebGL配置
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+	gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+	// 加载着色器并初始化属性缓冲区
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+	// 缓存数据
+	var bufferId = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( vertices ), gl.STATIC_DRAW );
+
+	// 将外部着色器变量与数据缓冲区关联
+	var vPosition = gl.getAttribLocation( program, "vPosition" );
+	gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition );
+
+	render();
+}
+
+function render(){
+	gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.drawArrays( gl.TRIANGLES, 0, 6 );
+}
